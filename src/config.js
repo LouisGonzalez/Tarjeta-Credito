4761cdd95c99a2f39e237ce933c3c3ad131f35de
Luis <louiscrack50@gmail.com>
2021-10-24 21:41:49 -0600

    credenciales para heroku

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
     database: {
-        username: "root",
-        password: "root",
-        database: "TARJETA_CREDITO",
-        host: "localhost"
+        username: "upaowqcwoynzag",
+        password: "96a3c949775d8d4dd13e2b09f3d5c85bdc0ef1810fe443d1aa938c3c2453d5a2",
+        database: "d7rh319tr7dhvm",
+        host: "ec2-52-7-159-155.compute-1.amazonaws.com",
+        port: 5432
     }
 }
