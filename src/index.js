9bad27e60fffc2900ff9f019a7ccee137893c19b
Luis <louiscrack50@gmail.com>
2021-10-27 23:28:17 -0600

    ....

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //todas las librarias y paquetes que se requieren
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const puerto = process.env.PORT || 8080
 const apiRouter = require('./routes/api')
 const cors = require("cors")
@@ -15,6 +16,15 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/front-end/dist/front-end/index.html'));
 });
 
+const corsOptions = {origin: "https://tarjeta-credito-seminario.herokuapp.com/"}
+app.use(cors({
+    origin: "https://tarjeta-credito-seminario.herokuapp.com/",
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser());
+
+
 
 
 app.use(cors())
@@ -25,10 +35,71 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
+app.use(express.static(__dirname + '/front-end'));
 
 app.listen(puerto, () => {
     console.log("Servidor Iniciado en el puerto" + puerto);
 });
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const puerto = process.env.PORT || 8080
+const apiRouter = require('./Routes/api.js')
+const cors = require("cors")
+
+const app = express();
+require('./Db');
+
+
+
+app.use(express.static(__dirname+'/front-end/dist/front-end'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/front-end/dist/front-end/index.html'));
+});
+
+
+
+const corsOptions = {origin: "https://tarjeta-credito-seminario.herokuapp.com/"}
+app.use(cors({
+    origin: "https://tarjeta-credito-seminario.herokuapp.com/",
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", apiRouter);
+
+app.use(express.static(__dirname + '/front-end'));
+
+app.listen(puerto, () => {
+    console.log("Servidor Iniciado en el puerto" + puerto);
+});
+
+
+
+
+
+
+
+
+
 
 /*//Base de datos
 const sequelize = require('./Db');
