ad30017daf66686be025bcbcc2356385725b1a68
Luis <louiscrack50@gmail.com>
2021-10-27 23:55:52 -0600

    asfd

--- src/Db.js
+++ src/Db.js
@@ -5,7 +5,18 @@ const sequelize = new Sequelize(
     database.username,
     database.password, {
     host: database.host,
-    dialect: "postgresql"
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            required:true,
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
 }
 );
 
