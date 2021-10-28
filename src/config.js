b8cd5f33e7a828f8b2d3e794a2c38f8cb03c722e
Luis <louiscrack50@gmail.com>
2021-10-27 23:53:44 -0600

    asdf

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ module.exports = {
         password: "96a3c949775d8d4dd13e2b09f3d5c85bdc0ef1810fe443d1aa938c3c2453d5a2",
         database: "d7rh319tr7dhvm",
         host: "ec2-52-7-159-155.compute-1.amazonaws.com",
-        port: 5432
+        port: 5432,
+        ssl: true
         }
 }
